d015d2a367d377bc748706128f3122359a473ba7
Peled12 <korenpeled08@gmail.com>
2025-03-08 19:47:06 +0200

    comment change

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exeption.filter';
 
-/*
-  TODO: create authentication logic (including all the security stuff)
-*/
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
